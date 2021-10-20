cc4706d23a2c86799f021b4a5650a4aa0b14aa0b
Harshada Samant <harshada.samant@rehau.com>
2021-10-20 08:33:55 +0530

    updated index

--- index.js
+++ index.js
@@ -1,31 +1,54 @@
-function onNameAdd() {
-	userName = document.getElementById('inputElement').value;
+var form = document.getElementById('form');
+var listGroup = document.getElementById('list-group');
 
-	appendNameToList(userName);
+// form submit event
+form.addEventListener('submit', addName);
+
+// removeName
+listGroup.addEventListener('click', removeName);
+
+//add Name
+
+function addName(e) {
+	e.preventDefault();
+
+	//getting the input value
+	var newName = document.getElementById('inputElement').value;
+
+	//creating a new li element
+	var li = document.createElement('li');
+
+	//adding class to li
+	li.classList.add('list-item');
+
+	//creating texnode to append it to li
+	li.appendChild(document.createTextNode(newName));
+
+	// creating delete button
+	var deleteBtn = document.createElement('button');
+
+	//adding class to delete button
+	deleteBtn.className = 'deleteBtn';
+
+	// inserting the text node to button
+	deleteBtn.appendChild(document.createTextNode('delete'));
+
+	// append button to li
+	li.appendChild(deleteBtn);
+
+	// adding li to listGroup
+	listGroup.appendChild(li);
+
+	// clearing the input field
 	document.getElementById('inputElement').value = '';
 }
 
-function appendNameToList(userName) {
-	// creating elements
-	listItem = document.createElement('li');
-	listDiv = document.createElement('div');
-	// adding class to list item
-	listItem.classList.add('list-item');
-
-	nameDiv = document.createElement('div');
-	deleteBtn = document.createElement('button');
-
-	nameDiv.innerHTML = userName;
-	deleteBtn.innerHTML = 'Delete';
-	deleteBtn.classList.add('deleteBtn');
-
-	deleteBtn.addEventListener('click', function () {
-		this.parentElement.parentElement.remove();
-	});
-
-	// get the list group
-	listGroup = document.getElementById('list-group');
-	listGroup.appendChild(listItem);
-	listItem.appendChild(listDiv);
-	listDiv.append(nameDiv, deleteBtn);
+// remove name
+function removeName(e) {
+	if (e.target.classList.contains('deleteBtn')) {
+		if (confirm('Are you sure ?')) {
+			var li = e.target.parentElement;
+			listGroup.removeChild(li);
+		}
+	}
 }
